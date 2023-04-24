0fe87cdca58abc3a96454240d1494d235c79f1c2
salahsaeed19 <salahsaef40@gmail.com>
2023-04-24 19:09:56 +0300

    2 version

--- boards/views.py
+++ boards/views.py
@@ -1,16 +1,11 @@
-# from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse
 from .models import Board, Post, Topic
-from django.http import HttpResponse
 
 
 def home(request):
+
     boards = Board.objects.all()
-    boards_name = []
-    for board in boards:
-        boards_name.append(board.name)
-    response_html = "<br>".join(boards_name)
-    return HttpResponse(response_html)
-    # return HttpResponse("<b>Salah is Backend developer with python django #</b>")
+    return render(request, "home.html", {'boards': boards})
 
 
 
